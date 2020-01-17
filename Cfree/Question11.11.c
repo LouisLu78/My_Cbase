1178dc7a904f95e42b305c4f54fd61cc5377544c
Louislu78 <lewgq@yahoo.com>
2020-01-17 20:41:21 +0800

    These are the modules I build for further usage.

---
+++ Cfree/Question11.11.c
@@ -0,0 +1,38 @@
+# include <stdio.h>
+
+struct person{
+    char lastname[15];
+    char firstname[15];
+    char age[4];
+};
+
+main()
+{
+    int i;
+    struct person Customer = {"unsigned","","0"};
+    FILE *fPtr;
+
+    if((fPtr = fopen("nameage.dat", "w")) == NULL){
+        printf("The file cannot be opened.");
+    }
+    else{
+        for (i = 0; i < 100; i++){            
+            fwrite(&Customer, sizeof(struct person), 1, fPtr);
+        }
+        fclose(fPtr);
+    }/*end of part a */
+
+    if((fPtr = fopen("nameage.dat", "r+")) == NULL){
+        printf("The file cannot be opened.");
+    }
+    else{
+        for (i = 0; i < 10; i++){
+        printf("Please enter the information of clients:"); 
+        scanf("%s%s%s", Customer.lastname, Customer.firstname, Customer.age);
+        fprintf(fPtr, "%s %s %s\n", Customer.lastname, Customer.firstname, Customer.age);
+        }
+        fclose(fPtr);
+    }/*end of part b */
+
+    return 0;
+}
