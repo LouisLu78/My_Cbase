51360353c397813f8304dd81452b9590c577c9be
Louislu78 <lewgq@yahoo.com>
2020-02-05 21:13:59 +0800

    My VSCommunity2019

--- Question18_10/Q18_10.cpp
+++ Question18_10/Q18_10.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 
-
 using namespace std;
 
 class IntegerSet
@@ -8,66 +7,51 @@ class IntegerSet
 private:
 	
 	int length;
-	bool a[];
+	bool a[101];
 
 public:
-	IntegerSet(bool set[], int size)
+
+	IntegerSet()
+	{
+		int i;
+		length = 101;
+		for (i = 0; i < length; i++) {
+			a[i] = false;
+		}
+	}
+
+	IntegerSet(bool s[], int size)
 	{
 		int i;
 		length = size;
 		for (i = 0; i < length; i++) {
-			a[i] = set[i];
+			a[i] = s[i];
+		}
+		for (i = length; i < 101; i++) {
+			a[i] = false;
 		}
 	}
 
-	void unionOfSets(IntegerSet aa, IntegerSet bb)
+	void unionOfSets(IntegerSet m, IntegerSet n)
 	{
 		int i;
-		if (aa.length < bb.length) {
-			length = bb.length;
-
-			for (i = 0; i < aa.length; i++) {
-				if (aa.a[i] == true || bb.a[i] == true) {
-					a[i] = true;
-				}
-				else {
-					a[i] = false;
-				}
+		
+		for (i = 0; i < 101; i++) {
+			if (m.a[i] == true || n.a[i] == true) {
+				a[i] = true;
 			}
-			for (i = aa.length; i < length; i++) {
-				a[i] = bb.a[i];
-			}
-		}
-		else {
-			length = aa.length;
-
-			for (i = 0; i < bb.length; i++) {
-				if (aa.a[i] == true || bb.a[i] == true) {
-					a[i] = true;
-				}
-				else {
-					a[i] = false;
-				}
-
-				for (i = bb.length; i < length; i++) {
-					a[i] = aa.a[i];
-				}
+			else {
+				a[i] = false;
 			}
 		}
 	}
 
-	void intersectionOfSets(IntegerSet aa, IntegerSet bb)
+	void intersectionOfSets(IntegerSet m, IntegerSet n)
 	{
 		int i;
 
-		if (aa.length < bb.length) {
-			length = aa.length;
-		}
-		else {
-			length = bb.length;
-		}
-		for (i = 0; i < length; i++) {
-			if (aa.a[i] == true && bb.a[i] == true) {
+		for (i = 0; i < 101; i++) {
+			if (m.a[i] == true && n.a[i] == true) {
 				a[i] = true;
 			}
 			else {
@@ -78,16 +62,81 @@ public:
 
 	void insertElement(int k)
 	{
-		a[k] = true;
+		if(k >= 0 && k <= 100){
+			a[k] = true;
+		}
+		else {
+			cout << "The number is not in the range from 0 to 100." << endl;
+		}
 	}
 
 	void deleteElement(int m)
 	{
-		a[m] = false;
+		if (m >= 0 && m <= 100) {
+			a[m] = false;
+		}
+		else {
+			cout << "The number is not in the range from 0 to 100." << endl;
+		}
 	}
 
 	void printSet() const
 	{
+		int i, flag = 0;
 
+		for (i = 0; i < length; i++) {
+			if (a[i]) {
+				flag = 1;
+				cout << i << " ";
+			}
+			else{
+				cout << "";
+			}
+		}
+		if (flag == 0) {
+			cout << "--";
+		}
+		cout << endl;
 	}
-};//to be finished tomorrow
+
+	int isEqualTo(IntegerSet m, IntegerSet n)
+	{
+		int i, flag = 1;
+		
+		for (i = 0; i < m.length; i++) {
+			if (m.a[i] != n.a[i]) {
+				flag = 0;
+				break;
+			}
+		}		
+		return flag;
+	}
+};
+
+int main()
+{
+	int i;
+	bool data[6] = {0,1,0,0,1,1};
+	bool test[3] = { 1,1,1 };
+	bool s[101] = { false };
+
+	IntegerSet iset1(s, 101);
+	iset1.printSet();
+
+	IntegerSet iset2;
+	iset2.printSet();
+
+	IntegerSet iset3(data, 6);	
+	iset3.printSet();
+
+	IntegerSet iset4(test, 3);
+	iset4.printSet();
+
+	iset2.unionOfSets(iset3, iset4);
+	iset2.printSet();
+
+	iset2.intersectionOfSets(iset3, iset4);
+	iset2.printSet();
+
+	return 0;
+}//I have solved this question.
