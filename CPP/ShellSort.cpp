617448aa99d42d85ba2def1bd6872712b4c38c1b
Louislu78 <lewgq@yahoo.com>
2020-05-02 11:27:09 +0800

    My codeblocks

---
+++ CPP/ShellSort.cpp
@@ -0,0 +1,57 @@
+//Author: Guangqiang Lu
+//Time:20200501
+//Email: gq4350lu@hotmail.com
+
+#include <stdio.h>
+#define SIZEA 11
+#define SIZEB 14
+
+void shellSort(int*, const int);
+void printArray(int*, const int);
+
+int main()
+{
+    int dataA[] = {5, 3, 7, 6, 4, 1, 0, 2, 9, 10, 8};
+    shellSort(dataA, SIZEA);
+    printArray(dataA, SIZEA);
+
+    int dataB[] = {2, 6, 4, 8, 100, 12, 89, 68, 314, 45, 37, 43, 456, 84};
+    shellSort(dataB, SIZEB);
+    printArray(dataB, SIZEB);
+
+    return 0;
+}
+
+void shellSort(int* array, const int size)
+{
+    int temp, d = size / 2;
+    while(d > 0){
+        for(int i = 0; i < d; i++ ){
+             for(int j = i; j < size - d; j += d ){
+                temp = array[j + d];
+                for (int k = j; k >= 0; k -= d){
+                     if (array[k] > temp)
+                    {
+                        array[k + d] = array[k];
+                        array[k] = temp;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+        d /= 2;
+    }
+}
+
+void printArray(int* array,  const int size)
+{
+    int i;
+    for ( i = 0; i < size; i++)
+    {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+}
